e35703c32b5e9df077d958ff39d089c1a82036c4
Asia Kuks <alexandra.rudenko@jetbrains.com>
2020-10-23 17:43:17 +0300

    Updated TODO

--- ActionsOnDependenciesPlayground/Plan.cs
+++ ActionsOnDependenciesPlayground/Plan.cs
@@ -2,11 +2,14 @@
 {
     public class Plan
     {
+        // TODO a. Core
+        // TODO b. Framework
+        // TODO c. Net
         // TODO 1. Show usages on diagram: B / references / A
         // TODO 2. Find dependent code: B / references / A
         // TODO 3. Find dependent code (all projects): B / references / A
-        // TODO 4. Optimize references: D / references / A (no usages)
-        // TODO 5. Safe delete: D / references / A (no usages)
+        // TODO 4. Optimize references: C / references / A (there are usages)
+        // TODO 5. Safe delete: C / references / A (there are usages)
         // TODO 6. Optimize references: C / references / A (no usages)
         // TODO 7. Safe delete: C / references / A (no usages)
     }
