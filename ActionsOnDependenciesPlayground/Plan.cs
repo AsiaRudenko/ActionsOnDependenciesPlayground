a564c5b800266373ffa650032358849d769410fd
Asia Kuks <alexandra.rudenko@jetbrains.com>
2020-10-23 17:54:19 +0300

    Added references for multiselection in Solution Explorer

--- ActionsOnDependenciesPlayground/Plan.cs
+++ ActionsOnDependenciesPlayground/Plan.cs
@@ -8,8 +8,8 @@
         // TODO 1. Show usages on diagram: B / references / A
         // TODO 2. Find dependent code: B / references / A
         // TODO 3. Find dependent code (all projects): B / references / A
-        // TODO 4. Optimize references: C / references / A (there are usages)
-        // TODO 5. Safe delete: C / references / A (there are usages)
+        // TODO 4. Optimize references: C / references / A and B (there are usages of A)
+        // TODO 5. Safe delete: C / references / A and B (there are usages of A)
         // TODO 6. Optimize references: C / references / A (no usages)
         // TODO 7. Safe delete: C / references / A (no usages)
     }
